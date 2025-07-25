1c6138596cefed3b82cfe42879a53aeef65692b4
CR7MAXJR <mohammedhatim490@gmail.com>
2025-07-25 16:14:51 +0300

    Update owner-fetch.js

--- plugins/owner-fetch.js
+++ plugins/owner-fetch.js
@@ -2,29 +2,35 @@ import fetch from 'node-fetch'
 import { format } from 'util'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (m.fromMe) return
-if (!/^https?:\/\//.test(text)) return m.reply(`Ejemplo:\n${usedPrefix + command} https://skyultraplus.com`)
-m.react("💻")
-let url = text
-let res = await fetch(url)
-if (res.headers.get('content-length') > 100 * 1024 * 1024 * 1024) {
-throw `Content-Length: ${res.headers.get('content-length')}`
+  if (m.fromMe) return
+  if (!/^https?:\/\//.test(text)) return m.reply(`📌 مثال:\n${usedPrefix + command} https://skyultraplus.com`)
+  
+  m.react("💻")
+  
+  let url = text
+  let res = await fetch(url)
+
+  if (res.headers.get('content-length') > 100 * 1024 * 1024 * 1024) {
+    throw `📦 الحجم كبير جدًا:\nContent-Length: ${res.headers.get('content-length')}`
+  }
+
+  if (!/text|json/.test(res.headers.get('content-type')))
+    return conn.sendFile(m.chat, url, 'file', text, m)
+
+  let txt = await res.buffer()
+
+  try {
+    txt = format(JSON.parse(txt + ''))
+  } catch (e) {
+    txt = txt + ''
+  } finally {
+    m.reply(txt.slice(0, 65536) + '')
+  }
 }
-    
-if (!/text|json/.test(res.headers.get('content-type'))) return conn.sendFile(m.chat, url, 'file', text, m)
-let txt = await res.buffer()
-try {
-txt = format(JSON.parse(txt + ''))
-} catch (e) {
-txt = txt + ''
-} finally {
-m.reply(txt.slice(0, 65536) + '')
-}
-}
-handler.help = ['fetch'].map(v => v + ' *<url>*')
-handler.tags = ['owner']
-handler.command = /^(fetch|get)$/i
-//handler.limit = 1
+
+handler.help = ['جلب *<رابط>*']
+handler.tags = ['المالك']
+handler.command = /^جلب$/i
 handler.register = true
 
 export default handler
