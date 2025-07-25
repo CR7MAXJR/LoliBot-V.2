138115b4aa4666c674329ced0c1b5f20842b15ff
CR7MAXJR <mohammedhatim490@gmail.com>
2025-07-25 07:41:42 +0300

    Update grupo-setname.js

--- plugins/grupo-setname.js
+++ plugins/grupo-setname.js
@@ -1,22 +1,18 @@
-//import Presence from '@adiwajshing/baileys'
-//let Presence = (await import(global.baileys)).default
-let handler  = async (m, { conn, args, text }) => {
-const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './media/Menu1.jpg'
-if (!text) throw "⚠️ Ingresar el texto para el grupo"
-try {
-let text = args.join` `
-if(!args || !args[0]) {
-} else {
-conn.groupUpdateSubject(m.chat, text)
+let handler = async (m, { conn, text }) => {
+  if (!text) throw '⚠️ الرجاء كتابة اسم جديد للمجموعة.'
+  try {
+    await conn.groupUpdateSubject(m.chat, text)
+    m.reply('✅ تم تغيير اسم المجموعة بنجاح.')
+  } catch (e) {
+    throw '❌ فشل في تغيير الاسم. تأكد أن لدي صلاحية الأدمن.'
+  }
 }
-m.react("✅️")
-} catch (e) { 
-throw "error"
-}}
-handler.help = ['setname'];
-handler.tags = ['group'];
-handler.command = /^(setname|newnombre|nuevonombre)$/i
+
+handler.help = ['اسم_المجموعة <الاسم الجديد>']
+handler.tags = ['group']
+handler.command = /^(setname|newnombre|nuevonombre|اسم_المجموعة|تغيير_الاسم)$/i
 handler.group = true
 handler.admin = true
 handler.botAdmin = true
-export default handler 
+
+export default handler
