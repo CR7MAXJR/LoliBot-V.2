0ad060b2ab6297966b537c2e365458e88d6def81
CR7MAXJR <mohammedhatim490@gmail.com>
2025-07-25 09:29:35 +0300

    Update sticker-exif.js

--- plugins/sticker-exif.js
+++ plugins/sticker-exif.js
@@ -1,25 +1,27 @@
-import { db } from '../lib/postgres.js';
+import { db } from '../lib/postgres.js'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) return m.reply(`*⚠️ Uso:* ${usedPrefix}${command} packname | author\n*Ejemplo:* ${usedPrefix}${command} LoliBot | elrebelde21`)
+  if (!args[0]) return m.reply(`*⚠️ الاستخدام:* ${usedPrefix}${command} اسم_الحزمة | المؤلف\n*مثال:* ${usedPrefix}${command} لولي_بوت | elrebelde21`)
 
-let text = args.join(' ').split('|');
-let packname = text[0].trim();
-let author = text[1] ? text[1].trim() : '';
+  let text = args.join(' ').split('|')
+  let packname = text[0].trim()
+  let author = text[1] ? text[1].trim() : ''
 
-if (!packname) return m.reply('⚠️ Debes ingresar al menos un *packname*.');
-if (packname.length > 600) return m.reply('⚠️ El *packname* es demasiado largo (máximo 600 caracteres).');
-if (author && author.length > 650) return m.reply('⚠️ El *author* es demasiado largo (máximo 650 caracteres).');
+  if (!packname) return m.reply('⚠️ يجب إدخال *اسم الحزمة* على الأقل.')
+  if (packname.length > 600) return m.reply('⚠️ اسم الحزمة طويل جدًا (الحد الأقصى 600 حرف).')
+  if (author && author.length > 650) return m.reply('⚠️ اسم المؤلف طويل جدًا (الحد الأقصى 650 حرف).')
 
-await db.query(`UPDATE usuarios
-      SET sticker_packname = $1,
-          sticker_author = $2
-      WHERE id = $3`, [packname, author || null, m.sender]);
-await m.reply(`✅ Perfecto, hemos actualizado el *EXIF* de tus stickers. Ahora cada sticker que crees tendrá:\n\n◉ *Packname:* ${packname}\n◉ *Author:* ${author || 'Ninguno'}\n\n> ¡A crear stickers personalizados! 😎`)
-};
-handler.help = ['exif <packname> | <author>'];
-handler.tags = ['sticker'];
-handler.command = ['exif'];
-handler.register = true;
+  await db.query(`UPDATE usuarios
+    SET sticker_packname = $1,
+        sticker_author = $2
+    WHERE id = $3`, [packname, author || null, m.sender])
 
-export default handler;
+  await m.reply(`✅ تم تحديث *البيانات التعريفية (EXIF)* لملصقاتك بنجاح.\n\n◉ *اسم الحزمة:* ${packname}\n◉ *المؤلف:* ${author || 'لا يوجد'}\n\n> يمكنك الآن إنشاء ملصقاتك المخصصة! 😎`)
+}
+
+handler.help = ['اكسيف <اسم_الحزمة> | <المؤلف>']
+handler.tags = ['sticker']
+handler.command = ['اكسيف']
+handler.register = true
+
+export default handler
