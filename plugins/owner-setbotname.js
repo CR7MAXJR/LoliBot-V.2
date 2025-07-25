16087717d39f44424c649a2bb2b075410964e3ec
CR7MAXJR <mohammedhatim490@gmail.com>
2025-07-25 16:27:31 +0300

    Update owner-setbotname.js

--- plugins/owner-setbotname.js
+++ plugins/owner-setbotname.js
@@ -1,17 +1,19 @@
 import { db } from "../lib/postgres.js";
 
 const handler = async (m, { args, conn }) => {
-const id = conn.user?.id;
-if (!id) return;
-const name = args.join(" ").trim();
-if (!name) return m.reply("❌ Escribe un nombre para el bot.\n\nEjemplo:\n/setbotname LoliBot 😎");
-await db.query(`UPDATE subbots SET name = $1 WHERE id = $2`, [name, id.replace(/:\d+/, '')]);
-m.reply(`✅ Nombre del bot actualizado a:\n*${name}*`);
+  const id = conn.user?.id;
+  if (!id) return;
+  const name = args.join(" ").trim();
+  if (!name) return m.reply("❌ اكتب اسمًا جديدًا للبوت.\n\nمثال:\n.اسم-البوت لولي بوت 😎");
+  
+  await db.query(`UPDATE subbots SET name = $1 WHERE id = $2`, [name, id.replace(/:\d+/, '')]);
+  m.reply(`✅ تم تحديث اسم البوت إلى:\n*${name}*`);
 };
-handler.help = ["setbotname <name>"];
-handler.tags = ["jadibot"];
-handler.command = /^setbotname$/i;
-handler.register = true
+
+handler.help = ["اسم-البوت <الاسم>"];
+handler.tags = ["البوتات"];
+handler.command = /^اسم-البوت$/i;
+handler.register = true;
 handler.owner = true;
 
 export default handler;
