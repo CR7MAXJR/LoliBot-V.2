41848f8d4603174b8d87bf7bcb649cd870a67446
CR7MAXJR <mohammedhatim490@gmail.com>
2025-07-25 16:28:47 +0300

    Update owner-setlogo.js

--- plugins/owner-setlogo.js
+++ plugins/owner-setlogo.js
@@ -1,17 +1,25 @@
 import { db } from "../lib/postgres.js";
 
 const handler = async (m, { args, conn }) => {
-const id = conn.user?.id;
-if (!id) return;
-const url = args[0];
-if (!url || !url.startsWith("http")) return m.reply("❌ Proporcione una URL válida de imagen.\n\nEjemplo:\n/setlogo https://i.imgur.com/logo.jpg");
-await db.query(`UPDATE subbots SET logo_url = $1 WHERE id = $2`, [url, id.replace(/:\d+/, '')]);
-m.reply("✅ Foto/logo del bot actualizada correctamente.");
+  const id = conn.user?.id;
+  if (!id) return;
+
+  const url = args[0];
+  if (!url || !url.startsWith("http"))
+    return m.reply("❌ يرجى إرسال رابط صورة صحيح.\n\nمثال:\nتغيير-الشعار https://i.imgur.com/logo.jpg");
+
+  await db.query(`UPDATE subbots SET logo_url = $1 WHERE id = $2`, [
+    url,
+    id.replace(/:\d+/, '')
+  ]);
+
+  m.reply("✅ تم تحديث شعار/صورة البوت بنجاح.");
 };
-handler.help = ["setlogo <url>"];
-handler.tags = ["jadibot"];
-handler.command = /^setlogo$/i;
-handler.register = true
+
+handler.help = ["تغيير-الشعار <الرابط>"];
+handler.tags = ["المالك"];
+handler.command = /^تغيير-الشعار$/i;
+handler.register = true;
 handler.owner = true;
 
 export default handler;
