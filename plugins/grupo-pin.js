800911537a369728c737be5a6f22ee7fe2f373dc
CR7MAXJR <mohammedhatim490@gmail.com>
2025-07-25 07:18:26 +0300

    Update grupo-pin.js

--- plugins/grupo-pin.js
+++ plugins/grupo-pin.js
@@ -1,41 +1,53 @@
 let handler = async (m, { conn, command }) => {
-if (!m.quoted) return m.reply(`⚠️ Responde a un mensaje para ${command === 'pin' ? 'fijarlo' : 'desfijarlo'}.`);
-try {
-let messageKey = {remoteJid: m.chat,
-fromMe: m.quoted.fromMe,
-id: m.quoted.id,
-participant: m.quoted.sender
+  if (!m.quoted)
+    return m.reply(`⚠️ قم بالرد على رسالة لـ ${
+      command === 'تثبيت'
+        ? 'تثبيتها'
+        : command === 'الغاء_تثبيت'
+        ? 'إلغاء تثبيتها'
+        : command === 'تمييز'
+        ? 'تمييزها'
+        : 'إلغاء تمييزها'
+    }.`);
+
+  try {
+    let messageKey = {
+      remoteJid: m.chat,
+      fromMe: m.quoted.fromMe,
+      id: m.quoted.id,
+      participant: m.quoted.sender,
+    };
+
+    if (command === 'تثبيت') {
+      await conn.sendMessage(m.chat, { pin: messageKey, type: 1, time: 604800 });
+      m.react("✅️");
+    }
+
+    if (command === 'الغاء_تثبيت') {
+      await conn.sendMessage(m.chat, { pin: messageKey, type: 2, time: 86400 });
+      m.react("✅️");
+    }
+
+    if (command === 'تمييز') {
+      await conn.sendMessage(m.chat, { keep: messageKey, type: 1, time: 15552000 });
+      m.react("✅️");
+    }
+
+    if (command === 'الغاء_تمييز') {
+      await conn.sendMessage(m.chat, { keep: messageKey, type: 2, time: 86400 });
+      m.react("✅️");
+    }
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-if (command === 'pin') {
-await conn.sendMessage(m.chat, { pin: messageKey,type: 1, time: 604800 })
-//conn.sendMessage(m.chat, {pin: {type: 1, time: 604800, key: messageKey }});
-m.react("✅️")
-}
-   
-if (command === 'unpin') {
-await conn.sendMessage(m.chat, { pin: messageKey,type: 2, time: 86400 })
-//conn.sendMessage(m.chat, { pin: { type: 0, key: messageKey }});
-m.react("✅️")
-}
+handler.help = ['تثبيت', 'الغاء_تثبيت', 'تمييز', 'الغاء_تمييز'];
+handler.tags = ['group'];
+handler.command = ['تثبيت', 'الغاء_تثبيت', 'تمييز', 'الغاء_تمييز'];
+handler.admin = true;
+handler.group = true;
+handler.botAdmin = true;
+handler.register = true;
 
-if (command === 'destacar') {
-conn.sendMessage(m.chat, {keep: messageKey, type: 1, time: 15552000 })
-m.react("✅️")
-}
-
-if (command === 'desmarcar') {
-conn.sendMessage(m.chat, {keep: messageKey, type: 2, time: 86400 })
-m.react("✅️")
-}
-} catch (error) {
-console.error(error);
-}};
-handler.help = ['pin']
-handler.tags = ['group']
-handler.command = ['pin', 'unpin', 'destacar', 'desmarcar'] 
-handler.admin = true
-handler.group = true
-handler.botAdmin = true
-handler.register = true 
-export default handler
+export default handler;
