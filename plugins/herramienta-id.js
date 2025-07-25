235dd48977f576335c031b95c7ed8ac81ce9e0d7
CR7MAXJR <mohammedhatim490@gmail.com>
2025-07-25 07:52:42 +0300

    Update herramienta-id.js

--- plugins/herramienta-id.js
+++ plugins/herramienta-id.js
@@ -1,10 +1,17 @@
 let handler = async (m, { conn, text, isOwner }) => {
-let USER_ID = m.user.lid 
-conn.fakeReply(m.chat, USER_ID, '0@s.whatsapp.net', `👇 AQUI ESTA TU NUMERO OCULTO "LID" 👇`, 'status@broadcast')
-//m.reply(USER_ID)
+  let USER_ID = m.user.lid 
+  conn.fakeReply(
+    m.chat, 
+    USER_ID, 
+    '0@s.whatsapp.net', 
+    `👇 إليك رقمك السري (LID) 👇`, 
+    'status@broadcast'
+  )
+  // m.reply(USER_ID) // تم التعليق عليه حسب الكود الأصلي
 }
-handler.help = ['mylid'];
-handler.tags = ['tools'];
-handler.command = /^mylid$/i;
+
+handler.help = ['رقمي_السري']
+handler.tags = ['أدوات']
+handler.command = /^رقمي_السري$/i
 
 export default handler;
