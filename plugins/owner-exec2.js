9b0a1d2ba9455dd0b3a13165d7c86e2104836012
CR7MAXJR <mohammedhatim490@gmail.com>
2025-07-25 16:13:55 +0300

    Update owner-exec2.js

--- plugins/owner-exec2.js
+++ plugins/owner-exec2.js
@@ -1,31 +1,28 @@
-import cp, { exec as _exec } from 'child_process';
-import { promisify } from 'util';
+import cp, { exec as _exec } from 'child_process'
+import { promisify } from 'util'
 
-let exec = promisify(_exec).bind(cp);
+let exec = promisify(_exec).bind(cp)
 
 let handler = async (m, { conn, isROwner }) => {
-  if (!isROwner) return;
+  if (!isROwner) return; // هذا الأمر فقط للمالك الحقيقي
   if (conn.user.jid !== conn.user.jid) return;
 
-  m.react("💻");
+  m.react("💻")
 
-  let commandInput = m.originalText?.replace(/^\$+\s?/, '').trim();
-  let o;
+  let الأمر = m.originalText?.replace(/^تشغيل\s?/, '').trim()
+  let النتيجة;
   try {
-    o = await exec(commandInput);
-  } catch (e) {
-    o = e;
+    النتيجة = await exec(الأمر)
+  } catch (خطأ) {
+    النتيجة = خطأ
   } finally {
-    let { stdout, stderr } = o;
-    if (stdout?.trim()) m.reply(stdout);
-    if (stderr?.trim()) m.reply(stderr);
+    let { stdout, stderr } = النتيجة
+    if (stdout?.trim()) m.reply(stdout)
+    if (stderr?.trim()) m.reply(stderr)
   }
-};
+}
 
-handler.help = ['$'];
-handler.tags = ['owner'];
-handler.customPrefix = /^[$]\s?/;
-handler.command = () => true; 
-// handler.rowner = true;
-
-export default handler;
+handler.help = ['تشغيل']
+handler.tags = ['المالك']
+handler.command = /^تشغيل\s?/i // الأمر يكون: تشغيل <كود شل>
+export default handler
