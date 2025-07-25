f5b12d938eba0e5f73c520964aa841dbed63866a
CR7MAXJR <mohammedhatim490@gmail.com>
2025-07-25 07:35:51 +0300

    Update grupo-resetLink.js

--- plugins/grupo-resetLink.js
+++ plugins/grupo-resetLink.js
@@ -1,12 +1,14 @@
-const handler = async (m, {conn}) => {
-const revoke = await conn.groupRevokeInvite(m.chat);
-await conn.reply(m.chat, `*_Se restableció con éxito el link del grupo._*\n*• Link Nuevo:* ${'https://chat.whatsapp.com/' + revoke}`, m);
+const handler = async (m, { conn }) => {
+  const revoke = await conn.groupRevokeInvite(m.chat);
+  await conn.reply(m.chat, `*_تم إعادة تعيين رابط الدعوة بنجاح._*\n*• الرابط الجديد:* ${'https://chat.whatsapp.com/' + revoke}`, m);
 };
-handler.help = ['resetlink']
-handler.tags = ['group']
-handler.command = ['resetlink', 'revoke'];
+
+handler.help = ['اعادة_الرابط'];
+handler.tags = ['group'];
+handler.command = ['اعادة_الرابط', 'اعادةالرابط', 'رابط_جديد'];
 handler.botAdmin = true;
 handler.admin = true;
 handler.group = true;
-handler.register = true 
+handler.register = true;
+
 export default handler;
