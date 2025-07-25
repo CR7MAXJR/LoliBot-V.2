1e22701de857fa2080fc9bf67c3f863c60de26ef
CR7MAXJR <mohammedhatim490@gmail.com>
2025-07-25 16:33:22 +0300

    Update owner-unbanchat.js

--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -1,19 +1,18 @@
 import { db } from "../lib/postgres.js";
 
 const handler = async (m, { conn }) => {
-    await db.query(`
-      INSERT INTO group_settings (group_id, banned)
-      VALUES ($1, false)
-      ON CONFLICT (group_id) DO UPDATE SET banned = false
-    `, [m.chat]);
+  await db.query(`
+    INSERT INTO group_settings (group_id, banned)
+    VALUES ($1, false)
+    ON CONFLICT (group_id) DO UPDATE SET banned = false
+  `, [m.chat]);
 
- m.reply("? Este grupo ha sido *desbaneado*. El bot volver? a responder aqu?.");
+  m.reply("✅ تم *إلغاء حظر* هذا الجروب، البوت سيعود للعمل هنا.");
 }
 
-handler.help = ['unbanchat'];
-handler.tags = ['owner']
-handler.command = /^unbanchat$/i
-//handler.botAdmin = true
-handler.owner = true
+handler.help = ['الغاء-الحظر'];
+handler.tags = ['المالك'];
+handler.command = /^الغاء-الحظر$/i;
+handler.owner = true;
 
-export default handler
+export default handler;
