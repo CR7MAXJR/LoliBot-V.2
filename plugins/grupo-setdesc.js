a62db7ebb350ce730dd807907d7d67ff27760693
CR7MAXJR <mohammedhatim490@gmail.com>
2025-07-25 07:39:33 +0300

    Update grupo-setdesc.js

--- plugins/grupo-setdesc.js
+++ plugins/grupo-setdesc.js
@@ -1,12 +1,14 @@
 let handler = async (m, { conn, args }) => {
-const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => 'https://telegra.ph/file/2a1d71ab744b55b28f1ae.jpg')
-await conn.groupUpdateDescription(m.chat, `${args.join(" ")}`);
-m.react("✅️")
+  const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => 'https://telegra.ph/file/2a1d71ab744b55b28f1ae.jpg')
+  await conn.groupUpdateDescription(m.chat, `${args.join(" ")}`);
+  m.react("✅️")
 }
-handler.help = ['setdesc'];
+
+handler.help = ['الوصف'];
 handler.tags = ['group'];
-handler.command = /^setdesk|setdesc|newdesc|descripción|descripcion$/i
-handler.group = true
-handler.admin = true
-handler.botAdmin = true
-export default handler
+handler.command = /^(setdesk|setdesc|newdesc|descripción|descripcion|الوصف)$/i;
+handler.group = true;
+handler.admin = true;
+handler.botAdmin = true;
+
+export default handler;
